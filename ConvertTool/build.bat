7e6884ddd5149180206f3bd1463149986f9bb723
jasonxiong <xiong123_zheng@163.com>
2019-01-13 20:02:43 +0800

    更新生成Lua Table配置文件工具

--- ConvertTool/build.bat
+++ ConvertTool/build.bat
@@ -2,6 +2,8 @@
 
 serverbin\dist\ConvertTool.exe -f  ≤∂”„≈‰÷√±Ì.xlsx -t server
 serverbin\dist\ConvertTool.exe -f  ≤∂”„≈‰÷√±Ì.xlsx -t client
+serverbin\dist\ConvertTool.exe -f  ≤∂”„≈‰÷√±Ì.xlsx -t server_lua
 serverbin\dist\MergeConfig.exe -f  server\ -t server\Config.zip
+serverbin\dist\MergeConfig.exe -f  server_lua\ -t server_lua\server_config.lua
 
 pause
