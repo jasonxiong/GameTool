ac41e57d598204548315cbec052b15c00b5b9b1e
jasonxiong <xiong123_zheng@163.com>
2019-01-13 19:54:11 +0800

    增加转换配置文件到lua table的功能

--- ConvertTool/serverbin/ConvertTool.py
+++ ConvertTool/serverbin/ConvertTool.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python
-#-*- coding: UTF-8 -*-
-#配置转换工具
+# -*- coding: UTF-8 -*-
+# 配置转换工具
 
 import os
 import sys
 import re
-from optparse import OptionParser 
+from optparse import OptionParser
 import json
 import time
 import xlwt
 import xlrd
 import collections
 from xml.dom.minidom import Document
+import types
 
-#Usage:
+# Usage:
 #          ./ConvertTool.py -f <from_excel> -t <type>
-#Example:  ./ConvertTool.py -f source.xls -t client
+# Example:  ./ConvertTool.py -f source.xls -t client
 
-#输入参数说明：
+# 输入参数说明：
 # -f 要转换的配置Excel文件名
 # -t 转换的配置类型， client 为客户端， server 为服务器
 
+
 def ConvertConfig():
-    
+
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    #1.读取命令行参数
-    parser = OptionParser() 
-    parser.add_option("-f", "--fromfile", action="store", dest="fromfile", help="要转换的Excel文件名") 
-    parser.add_option("-t", "--type", action="store", dest="convtype", help="转换的类型，客户端或服务器") 
-    (options, args) = parser.parse_args()       
+    # 1.读取命令行参数
+    parser = OptionParser()
+    parser.add_option("-f", "--fromfile", action="store",
+                      dest="fromfile", help="要转换的Excel文件名")
+    parser.add_option("-t", "--type", action="store",
+                      dest="convtype", help="转换的类型，客户端或服务器或lua")
+    (options, args) = parser.parse_args()
 
-    if options.fromfile is None: 
-        parser.error("please input config excel file.") 
+    if options.fromfile is None:
+        parser.error("please input config excel file.")
         return
 
     if options.convtype is None:
@@ -43,7 +47,7 @@ def ConvertConfig():
     fromfile = options.fromfile
     convtype = options.convtype
 
-    CONFIG_DIR='config/'
+    CONFIG_DIR = 'config/'
 
     if convtype == "client":
         CONFIG_FILE = 'client.json'
@@ -51,31 +55,37 @@ def ConvertConfig():
     elif convtype == "server":
         CONFIG_FILE = 'server.json'
         CONF_TO_DIR = 'server/'
+    elif convtype == 'server_lua':
+        CONFIG_FILE = 'server_lua.json'
+        CONF_TO_DIR = 'server_lua/'
+
     else:
-        print("invalid convtype %s\n"%(convtype))
+        print("invalid convtype %s\n" % (convtype))
         return
 
-    #2.读取配置文件
+    # 2.读取配置文件
     jsonData = None
     with open(CONFIG_DIR+CONFIG_FILE, 'r') as JsonConfFile:
         jsonData = json.load(JsonConfFile)
 
-    #3.读取Excel文件
+    # 3.读取Excel文件
     excelData = xlrd.open_workbook(CONFIG_DIR+options.fromfile)
-    
-    #先生成服务器的
+
+    # 先生成服务器的
     print "------------------------ConvertConfig------------------------"
-    iRet = ConvertOneConfig(CONF_TO_DIR, jsonData["filelist"], excelData, jsonData["totype"])
+    iRet = ConvertOneConfig(
+        CONF_TO_DIR, jsonData["filelist"], excelData, jsonData["totype"])
     if iRet != 0:
-        print "Failed to convert %s file, ret %d"%(convtype, iRet)
+        print "Failed to convert %s file, ret %d" % (convtype, iRet)
         return
-    
+
     print "\n"
 
     '''
-    #再生成客户端的
+    # 再生成客户端的
     print "------------------------ClientConfig------------------------"
-    iRet = ConvertOneConfig(CLIENT_DIR, jsonData["client"]["filelist"], excelData, jsonData["client"]["totype"])
+    iRet = ConvertOneConfig(
+        CLIENT_DIR, jsonData["client"]["filelist"], excelData, jsonData["client"]["totype"])
     if iRet != 0:
         print "Failed to config client file, ret %d"%(iRet)
         return
@@ -83,7 +93,7 @@ def ConvertConfig():
     print "\n"
     '''
 
-    print "Success to convert %s config file!\n"%(convtype)
+    print "Success to convert %s config file!\n" % (convtype)
 
 def ConvertOneConfig(outDir, jsonData, excelData, tofiletype):
     for convertInfo in jsonData:
@@ -92,33 +102,33 @@ def ConvertOneConfig(outDir, jsonData, excelData, tofiletype):
         fp = file(tofile,'w')
         nColumns = table.ncols
 
-        #先确定所有字段在table中的位置
+        # 先确定所有字段在table中的位置
         dataIndexs = [-1] * len(convertInfo["data"])
         tableRowValues = table.row_values(0)
         for i in range(0, len(convertInfo["data"])):
             if convertInfo["data"][i]["name"] not in tableRowValues:
-                #要转换的字段不存在
+                # 要转换的字段不存在
                 print "table %s"%(convertInfo["fromfile"])+" : %s not exist!"%(convertInfo["data"][i]["name"])
                 continue
             
-            #确定字段在Excel表中的位置
+            # 确定字段在Excel表中的位置
             dataIndexs[i] = tableRowValues.index(convertInfo["data"][i]["name"])
 
-        #保存输出数据
+        # 保存输出数据
         if tofiletype == "json":
-            #转换成json
+            # 转换成json
             outData = []
 
-            #遍历Excel表中的行
+            # 遍历Excel表中的行
             for rowIndex in range(1, table.nrows):
                 oneOutData = collections.OrderedDict()
                 for i in range(0, len(dataIndexs)):
                     if dataIndexs[i] < 0:
-                        #找不到该字段
+                        # 找不到该字段
                         print "Failed to convert %s"%(convertInfo["fromfile"])+", no data %s"%(convertInfo["data"][i]["name"])
                         return -1
                     
-                    #写入数据
+                    # 写入数据
                     if convertInfo["data"][i]["type"] == "int":
                         # int 类型数据
                         oneOutData[convertInfo["data"][i]["name"]] = int(table.cell(rowIndex, dataIndexs[i]).value)
@@ -131,34 +141,34 @@ def ConvertOneConfig(outDir, jsonData, excelData, tofiletype):
                     
                 outData.append(oneOutData)
 
-            #输出数据到文件中
+            # 输出数据到文件中
             json.dump(outData, fp, indent=2)
 
         elif tofiletype == "xml":
-            #转换成xml
+            # 转换成xml
             outData = Document()
 
-            #创建根节点
+            # 创建根节点
             rootNode = outData.createElement('root')
             outData.appendChild(rootNode)
 
-            #遍历Excel表中的行
+            # 遍历Excel表中的行
             for rowIndex in range(1, table.nrows):
                 
-                #创建一个记录
+                # 创建一个记录
                 oneRecordNode = outData.createElement(convertInfo["fromfile"])
                 rootNode.appendChild(oneRecordNode)
 
                 for i in range(0, len(dataIndexs)):
                     if dataIndexs[i] < 0:
-                        #找不到该字段
+                        # 找不到该字段
                         print "Failed to convert %s"%(convertInfo["fromfile"])+", no data %s"%(convertInfo["data"][i]["name"])
                         return -1
                     
-                    #创建一列数据的节点
+                    # 创建一列数据的节点
                     oneData = outData.createElement(convertInfo["data"][i]["name"])
                     
-                    #写入数据内容
+                    # 写入数据内容
                     oneDataText = None
                     if convertInfo["data"][i]["type"] == "int":
                         # int 类型数据
@@ -172,15 +182,15 @@ def ConvertOneConfig(outDir, jsonData, excelData, tofiletype):
                     
                     oneData.appendChild(oneDataText)
 
-                    #插入到记录中
+                    # 插入到记录中
                     oneRecordNode.appendChild(oneData)
 
-            #输出数据到文件中
+            # 输出数据到文件中
             with open(tofile, 'w') as f:
                 f.write(outData.toprettyxml(indent="\t", encoding='utf-8'))
-
+                
         elif tofiletype == "bin":
-            #转换成二进制 todo jasonxiong
+            # 转换成二进制 todo jasonxiong
             pass
         else:
             "Failed to convert config, invalid tofiletype %s"%(tofiletype)
--- ConvertTool/serverbin/MergeConfig.py
+++ ConvertTool/serverbin/MergeConfig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
-#将多个配置json合并成1个
+#将多个配置json合并成1个Json配置或Lua配置
 
 import os
 import sys
@@ -9,6 +9,8 @@ from optparse import OptionParser
 import json
 import time
 import collections
+import types
+import string
 
 #Usage:
 #          ./MergeConfig.py -f <from_dir> -t <tofile>
@@ -18,6 +20,67 @@ import collections
 # -f 待合并的json文件目录
 # -t 转换到的目标文件名
 
+LUA_CONFIG_TEMPLATE_FILE = "server_lua/ConfigTemplate.lua"
+
+def SpaceStr(layer):
+    	lua_str = ""
+	for i in range(0, layer):
+		lua_str += '\t'
+	return lua_str
+
+ # json转换成lua table，返回lua table串，可能会返回空
+def JsonToLuaStr(data, layer=0):    
+    if isinstance(data,types.StringTypes) or isinstance(data,str) or isinstance(data,types.UnicodeType):
+		return "'" + data + "'"
+    elif isinstance(data,types.BooleanType):
+		if data:
+			return 'true'
+		else:
+			return 'false'
+    elif isinstance(data,types.IntType) or isinstance(data,types.LongType) or isinstance(data,types.FloatType):
+		return str(data)
+    elif isinstance(data,types.ListType):
+        lua_str = "{\n"
+        lua_str += SpaceStr(layer+1)
+        for i in range(0,len(data)): 
+            lua_str += JsonToLuaStr(data[i],layer+1)
+            if i < len(data)-1:
+                lua_str += ','
+        lua_str += '\n'
+        lua_str += SpaceStr(layer)
+        lua_str +=  '}'
+        return lua_str
+    elif isinstance(data,types.DictType) or isinstance(data, types.DictionaryType):
+        lua_str = ''
+        lua_str += "\n"
+        lua_str += SpaceStr(layer)
+        lua_str += "{\n"
+        data_len = len(data)
+        data_count = 0
+        for k,v in data.items():
+            data_count += 1
+            lua_str += SpaceStr(layer+1)
+            if type(k) is types.IntType:
+                lua_str += '[' + str(k) + ']'
+            else:
+                lua_str += k 
+            lua_str += ' = '
+            try:
+                lua_str += JsonToLuaStr(v,layer +1)
+                if data_count < data_len:
+                    lua_str += ',\n'
+
+            except Exception, e:
+                print 'error in ',k,v
+                return None
+        lua_str += '\n'
+        lua_str += SpaceStr(layer)
+        lua_str += '}'
+        return lua_str
+    else:
+		print type(data) , 'is error'
+		return None
+
 def ConvertConfig():
     
     reload(sys)
@@ -25,7 +88,7 @@ def ConvertConfig():
 
     #1.读取命令行参数
     parser = OptionParser() 
-    parser.add_option("-f", "--fromdir", action="store", dest="fromdir", help="待合并的json文件目录") 
+    parser.add_option("-f", "--fromdir", action="store", dest="fromdir", help="待合并的json文件目录和lua文件目录") 
     parser.add_option("-t", "--tofile", action="store", dest="tofile", help="转换到的目标文件名") 
     (options, args) = parser.parse_args()       
 
@@ -44,11 +107,24 @@ def ConvertConfig():
     jsonData = {}
     for onefile in os.listdir(fromdir):
         if os.path.isfile(os.path.join(fromdir,onefile)) and os.path.splitext(onefile)[1] == ".json":
-            jsonData[onefile] = json.load(open(os.path.join(fromdir, onefile)))
+            if "Config.zip" in tofilename:
+                jsonData[onefile] = json.load(open(os.path.join(fromdir, onefile)))
+            elif "server_config.lua" in tofilename:
+                jsonData[onefile.replace('.json','')] = json.load(open(os.path.join(fromdir, onefile)))
 
     #3.保存到文件
-    fp = file(tofilename,'w')
-    json.dump(jsonData, fp, indent=2)
+    if "Config.zip" in tofilename:
+        #转换成zip文件
+        fp = file(tofilename,'w')
+        json.dump(jsonData, fp, indent=2)
+    elif "server_config.lua" in tofilename:
+        #转换成lua配置
+        fpTemplate = file(LUA_CONFIG_TEMPLATE_FILE,'r')
+        fp = file(tofilename, 'w')
+        values = {'xdby_config_lua' : JsonToLuaStr(jsonData)}
+        fp.write(string.Template(fpTemplate.read()).substitute(values))
+    else:
+        print "Failed to merge config, invalid to file name %s"%tofilename
     
     print "\n"
 
